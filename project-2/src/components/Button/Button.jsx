c0986fd1bcab8bf8aeb1e82a737dee3c588f4da1
manixcoder <pathakmanish86@gmail.com>
2024-10-22 00:00:20 +0530

    onclick and button properties

--- project-2/src/components/Button/Button.jsx
+++ project-2/src/components/Button/Button.jsx
@@ -3,14 +3,13 @@ import React from 'react'
 import styles from './Button.module.css'
 const Button = (props) => {
     console.log(props);
-    const {} =props
-  return (
-    <button className={props.isOutline? styles.outline_btn : styles.primary_btn}>
-        
-        {props.icon}
-      {props.text}
-    </button>
-  )
+    const { isOutline, icon, text, ...rest } = props;
+    return (
+        <button {...rest} className={isOutline ? styles.outline_btn : styles.primary_btn}>
+            {icon}
+            {text}
+        </button>
+    )
 }
 
 export default Button
--- project-2/src/components/ContactForm/ContactForm.jsx
+++ project-2/src/components/ContactForm/ContactForm.jsx
@@ -6,18 +6,41 @@ import { FaPhoneAlt } from "react-icons/fa";
 import { HiMail } from "react-icons/hi";
 
 const ContactForm = () => {
+    const onViaCallSubmit=()=>{
+        console.log("I am from call");
+    }
     return (
         <section className={styles.container}>
             <div className={styles.contact_form}>
                 <div className={styles.top_btn}>
                     <Button text="VIA SUPPORT CHAT" icon={<MdMessage fontSize="24px" />} />
-                    <Button text="VIA CALL" icon={<FaPhoneAlt fontSize="24px" />} />
+                    <Button onClick={onViaCallSubmit} text="VIA CALL" icon={<FaPhoneAlt fontSize="24px" />} />
                 </div>
-                <Button 
-                isOutline={true}
-                text="VIA EMAI FORM" icon={<HiMail fontSize="24px" />} />
+                <Button isOutline={true} text="VIA EMAI FORM" icon={<HiMail fontSize="24px" />} />
+                <form>
+                    <div className={styles.form_control}>
+                        <label htmlFor="name">Name</label>
+                        <input type="text" name="name" id="name" />
+                    </div>
+                    <div className={styles.form_control}>
+                        <label htmlFor="email">Email</label>
+                        <input type="email" name="email" id="email" />
+                    </div>
+                    <div className={styles.form_control}>
+                        <label htmlFor="text">Text Message</label>
+                        <textarea name="text" rows="8" id="text" />
+                    </div>
+                    <div style={{
+                        display:'flex',justifyContent:'end'
+                    }}><Button text="Submit"  /></div>
+
+                </form>
             </div>
-            <div className={styles.cotact_image}></div>
+            <div className={styles.cotact_image}>
+                <img src='/images/contact.svg' alt='banner'/>
+            </div>
+
+
         </section>
     )
 }
