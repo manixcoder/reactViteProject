24b1c872d7be110e92277fa80bc8a0c6fc032631
manixcoder <pathakmanish86@gmail.com>
2024-10-22 19:46:04 +0530

    Dice selection and roledice

--- project-3-dice_game/src/components/GamePlay.jsx
+++ project-3-dice_game/src/components/GamePlay.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import TotalScore from './TotalScore';
+import NumberSelecter from './NumberSelecter';
+import RoleDice from './RoleDice';
 
 const GamePlay = () => {
-  return (
-    <div>
-      GamePlay
-    </div>
-  )
+    const [selectedNumber, setSelectedNumber] = useState();
+    return (
+        <MainContainer>
+            <div className='top_section'>
+                <TotalScore />
+                <NumberSelecter
+                    selectedNumber={selectedNumber}
+                    setSelectedNumber={selectedNumber}
+                />
+            </div>
+            <RoleDice />
+
+        </MainContainer>
+    )
 }
 
-export default GamePlay
+export default GamePlay;
+
+const MainContainer = styled.main`
+padding-top:70px;
+    .top_section{
+        display: flex;
+        justify-content: space-around;
+        align-items: end;
+    }
+`;
