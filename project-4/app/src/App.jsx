e606a89636ceaaae963e33c450c9e2d915d87d67
manixcoder <pathakmanish86@gmail.com>
2024-10-24 00:59:06 +0530

    Filter functionality 2

--- project-4/app/src/App.jsx
+++ project-4/app/src/App.jsx
@@ -1,43 +1,34 @@
-
-import { useEffect, useState } from 'react';
-import styled from 'styled-components'
-import SearchResult from './components/SearchResults/SearchResult';
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import SearchResult from "./components/SearchResults/SearchResult";
 
 export const BASE_URL = "http://localhost:9000";
 
-
-function App() {
-  const [filteredData, setFilteredData] = useState(null);
+const App = () => {
   const [data, setData] = useState(null);
-
+  const [filteredData, setFilteredData] = useState(null);
   const [loading, setLoading] = useState(false);
-
   const [error, setError] = useState(null);
-
-  const [selectedBtn,setSelectedBtn]=useState("all")
+  const [selectedBtn, setSelectedBtn] = useState("all");
 
   useEffect(() => {
     const fetchFoodData = async () => {
       setLoading(true);
+
       try {
         const response = await fetch(BASE_URL);
+
         const json = await response.json();
 
         setData(json);
         setFilteredData(json);
         setLoading(false);
-        console.log(json);
       } catch (error) {
-        setError("Unable to fetch data")
-        console.log(error);
+        setError("Unable to fetch data");
       }
-
-    }
+    };
     fetchFoodData();
   }, []);
-
-  console.log(data);
-
 
   const searchFood = (e) => {
     const searchValue = e.target.value;
@@ -51,15 +42,11 @@ function App() {
     const filter = data?.filter((food) =>
       food.name.toLowerCase().includes(searchValue.toLowerCase())
     );
-    setFilteredData(filter)
+    setFilteredData(filter);
+  };
 
-
-
-
-  }
-
-  const filterFood=(type)=>{
-    if(type==="all"){
+  const filterFood = (type) => {
+    if (type === "all") {
       setFilteredData(data);
       setSelectedBtn("all");
       return;
@@ -68,29 +55,31 @@ function App() {
     const filter = data?.filter((food) =>
       food.type.toLowerCase().includes(type.toLowerCase())
     );
-
     setFilteredData(filter);
-    selectedBtn(type);
+    setSelectedBtn(type);
   };
 
-  const filterBtns=[
+  const filterBtns = [
     {
-      name:"All",
-      type:"all"
+      name: "All",
+      type: "all",
     },
     {
-      name:"Breakfast",
-      type:"breakfast"
-    },{
-      name:"Lunch",
-      type:"lunch"
-    },{
-      name:"Dinner",
-      type:"dinner"
+      name: "Breakfast",
+      type: "breakfast",
     },
-  ]
+    {
+      name: "Lunch",
+      type: "lunch",
+    },
+    {
+      name: "Dinner",
+      type: "dinner",
+    },
+  ];
+
   if (error) return <div>{error}</div>;
-  if (loading) return <div>loading....</div>
+  if (loading) return <div>loading.....</div>;
 
   return (
     <>
@@ -99,46 +88,48 @@ function App() {
           <div className="logo">
             <img src="/logo.svg" alt="logo" />
           </div>
+
           <div className="search">
-            <input onChange={searchFood} type="text" className="text" placeholder='Search food' />
+            <input onChange={searchFood} placeholder="Search Food" />
           </div>
         </TopContainer>
-        <FilterContainer>
-          {
-            filterBtns.map((value)=>(
-              <Button 
-              isSelected={selectedBtn === value.type}
-              key={value.name} onClick={ ()=>filterFood(value.type)}>{value.name}</Button>
-            ))
-          }
 
-         
+        <FilterContainer>
+          {filterBtns.map((value) => (
+            <Button
+              isSelected={selectedBtn === value.type}
+              key={value.name}
+              onClick={() => filterFood(value.type)}
+            >
+              {value.name}
+            </Button>
+          ))}
         </FilterContainer>
       </Container>
       <SearchResult data={filteredData} />
     </>
-
-  )
-}
+  );
+};
 
 export default App;
-
 
 export const Container = styled.div`
   max-width: 1200px;
   margin: 0 auto;
 `;
 const TopContainer = styled.section`
-  min-height: 140px;
+  height: 140px;
   display: flex;
   justify-content: space-between;
   padding: 16px;
   align-items: center;
-  .search{
-    input{
+
+  .search {
+    input {
       background-color: transparent;
       border: 1px solid red;
       color: white;
+      border-radius: 5px;
       height: 40px;
       font-size: 16px;
       padding: 0 10px;
@@ -148,17 +139,17 @@ const TopContainer = styled.section`
     }
   }
 
-  @media(0 < width < 600px){
+  @media (0 < width < 600px) {
     flex-direction: column;
     height: 120px;
   }
 `;
 
 const FilterContainer = styled.section`
-    display: flex;
-    justify-content: center;
-    gap: 12px;
-    padding-bottom: 40px;
+  display: flex;
+  justify-content: center;
+  gap: 12px;
+  padding-bottom: 40px;
 `;
 
 export const Button = styled.button`
@@ -167,14 +158,9 @@ export const Button = styled.button`
   border-radius: 5px;
   padding: 6px 12px;
   border: none;
+  color: white;
   cursor: pointer;
-  &:hover{
+  &:hover {
     background-color: #f22f2f;
   }
 `;
-
-
-
-
-
-
