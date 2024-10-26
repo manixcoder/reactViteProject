2fa6b8184eedb32683dc46c6560dc67dc66fd1ab
manixcoder <pathakmanish86@gmail.com>
2024-10-26 23:03:20 +0530

    Firebase connection stablise and data fetched

--- project-5/src/App.jsx
+++ project-5/src/App.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './components/Navbar';
 import { FaSearch } from "react-icons/fa";
-import { AiFillPlusCircle } from 'react-icons/ai'
+import { AiFillPlusCircle } from 'react-icons/ai';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from './config/firebase';
 
 const App = () => {
+
+  const [contacts, setContacts] = useState([]);
+  useEffect(() => {
+    const getContacts = async () => {
+      try {
+        const contactsRef = collection(db, 'contacts');
+        const contactsSnapshot = await getDocs(contactsRef);
+        const contactLists = contactsSnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data()
+        }));
+        console.log(contactLists);
+        setContacts(contactLists);
+      } catch (error) {
+
+      }
+
+
+    };
+
+    getContacts();
+  }, []);
   return (
     <div className='max-w-[370px] mx-auto px-4'>
       <Navbar />
@@ -14,9 +38,9 @@ const App = () => {
             <input type="text" className='flex-grow text-white pl-9 border bg-transparent border-white h-10 my-2 rounded-lg'
             />
           </div>
-        
-            <AiFillPlusCircle className='text-5xl text-white cursor-pointer'/>
-          
+
+          <AiFillPlusCircle className='text-5xl text-white cursor-pointer' />
+
         </div>
       </div>
 
