fb65d482caa006fed597d20185ecfb779986f395
manixcoder <pathakmanish86@gmail.com>
2024-10-26 22:43:46 +0530

    Landing page design done

--- project-5/src/App.jsx
+++ project-5/src/App.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import Navbar from './components/Navbar';
+import { FaSearch } from "react-icons/fa";
+import { AiFillPlusCircle } from 'react-icons/ai'
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline flex">
-        Hello world!
-      </h1>
+    <div className='max-w-[370px] mx-auto px-4'>
+      <Navbar />
+      <div className='flex gap-2'>
+        <div className='flex relative items-center flex-grow'>
+          <FaSearch className='text-white ml-1 text-3xl absolute' />
+          <div className='flex'>
+            <input type="text" className='flex-grow text-white pl-9 border bg-transparent border-white h-10 my-2 rounded-lg'
+            />
+          </div>
+        
+            <AiFillPlusCircle className='text-5xl text-white cursor-pointer'/>
+          
+        </div>
+      </div>
+
     </div>
   )
 }
--- project-5/src/config/firebase.js
+++ project-5/src/config/firebase.js
@@ -1,20 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getFirestore } from 'firebase/firestore'
+import getFirestore from 'firebase/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCnQneJdrEnbA0iyIjpWINYVJFgjvQW3Kw",
-  authDomain: "vite-conatact.firebaseapp.com",
-  projectId: "vite-conatact",
-  storageBucket: "vite-conatact.appspot.com",
-  messagingSenderId: "241671078032",
-  appId: "1:241671078032:web:57acd70578e417dfa95815"
+  apiKey: "AIzaSyBt-ylvwASNUaplmcz0jocP_Pwvc4Z6LgM",
+  authDomain: "react-contact-61cee.firebaseapp.com",
+  projectId: "react-contact-61cee",
+  storageBucket: "react-contact-61cee.appspot.com",
+  messagingSenderId: "331223206383",
+  appId: "1:331223206383:web:d93b3d9122be8bf7ee4e1a"
 };
 
 // Initialize Firebase
+
 export const app = initializeApp(firebaseConfig);
 
 export const db= getFirestore(app);
